23d28216d752bebbf382ebecc73494506c6bdde0
Marilize Pires <50778867+MarilizePi@users.noreply.github.com>
2019-09-03 09:52:15 -0700

    Add files via upload

---
+++ DecimalComparator.java
@@ -0,0 +1,21 @@
+package com;
+
+public class DecimalComparator {
+    public static void main(String[] args) {
+        areEqualByThreeDecimalPlaces(3.1756, -3.175);
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double firstNumber,
+                                                       double secondNumber) {
+        int firstNumberInt = (int) (firstNumber * 1000);
+        int secondNumberInt = (int) (secondNumber * 1000);
+
+        if (firstNumberInt == secondNumberInt) {
+            System.out.println("The numbers are equal.");
+            return true;
+        } else {
+            System.out.println("The numbers are not equal.");
+            return false;
+        }
+    }
+}
