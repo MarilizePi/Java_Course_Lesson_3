82668f12e70d4a8a1abef35cce605e3202e73c9b
Marilize Pires <50778867+MarilizePi@users.noreply.github.com>
2019-09-04 09:15:11 -0700

    Add files via upload

---
+++ Project_6_EqualSum_Checker.java
@@ -0,0 +1,19 @@
+public class Project_6_EqualSum_Checker {
+    public static void main(String[] args) {
+        hasEqualSum(3,2,6);
+        hasEqualSum(2,-2,0);
+    }
+
+    public static boolean hasEqualSum(int firstNumber, int secondNumber,
+                                      int thirdNumber) {
+        if ((firstNumber + secondNumber) == thirdNumber) {
+            System.out.println(firstNumber + " + " + secondNumber +
+                    " = " + thirdNumber);
+            return true;
+        } else {
+            System.out.println(firstNumber + " + " +
+                    secondNumber + " does not equal " + thirdNumber);
+            return false;
+        }
+    }
+}
