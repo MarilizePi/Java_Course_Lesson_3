24663c93ecc90318e002f5132fbcb39e407935c6
Marilize Pires <50778867+MarilizePi@users.noreply.github.com>
2019-09-18 08:53:37 -0700

    Add files via upload

---
+++ Project_15_NumberPalindrome.java
@@ -0,0 +1,47 @@
+//A Palindrome number is a number which when reversed is equal to the original number.
+//Ex: 121, 12321, 1001, etc.
+
+//THE LOGIC BEHIND THIS CODE:
+/* First of all, I need a number to compare its reverse.
+ * Second, I can create a variable called "number".
+ * Third, I need another variable to store the remainder. I called it "remainder" :)
+ * Fourth, I need another variable to store the reversed number. I called it "reverseNumber.
+ * In order to calculate the remainder, I have to apply the module operator (%) to get the
+ * last digit.
+ * Fifth, I have to break down my number, so I remove the last digit and stay with the remaining number.
+ * Sixth, my reverse number is zero by default. So, multiplying zero and 10, I get zero, + my
+ * remainder, which is the "remainder" number.
+ * The loop will go on until it checks the whole number and it gives me the result: true or false.
+ * ATTENTION: I also need an "if statement" to compare "number" and "reverseNumber". For this,
+ * I created a variable called "compareInputAndOutput and set it equal to number, because we
+ * want the result to be true.*/
+
+
+public class Project_15_NumberPalindrome {
+
+    public static void main(String[] args) {
+        isPalindrome(121);
+        isPalindrome(123);
+        isPalindrome(-222);
+    }
+
+    public static boolean isPalindrome(int number) {
+        int remainder;
+        int reverseNumber = 0;
+        int compareInputAndOutput = number;
+
+        while (number != 0) {
+            remainder = number % 10;
+            number = number / 10;
+            reverseNumber = (reverseNumber * 10) + remainder;
+        }
+
+        if (compareInputAndOutput == reverseNumber) {
+            System.out.println("The number " + number + " is Palindrome");
+            return true;
+        } else {
+            System.out.println("The number " + number + " is NOT Palindrome");
+            return false;
+        }
+    }
+}
