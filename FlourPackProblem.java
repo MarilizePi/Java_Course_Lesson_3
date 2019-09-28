69c224768f7ea85df48041fcc68d7ff134cb8e44
Marilize Pires <50778867+MarilizePi@users.noreply.github.com>
2019-09-28 09:53:23 -0700

    Add files via upload

---
+++ FlourPackProblem.java
@@ -0,0 +1,25 @@
+public class FlourPackProblem {
+    public static void main(String[] args) {
+        canPack(1, 0, 4);
+        canPack(1, 0, 5);
+        canPack(0, 5, 4);
+        canPack(2, 2, 11);
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        int bigCountWeightKg = 5;
+        int smallCountWeightKg = 1;
+
+        int result = ((bigCountWeightKg * bigCount) % goal);  // (5 * 1) % 5
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        } else if ((bigCountWeightKg * bigCount) + (smallCountWeightKg * smallCount) < goal) {
+            return false;
+        } else if ((result != 0) && ((smallCountWeightKg * smallCount) + result) < 5) {
+            return false;
+        }
+        return true;
+    }
+}
