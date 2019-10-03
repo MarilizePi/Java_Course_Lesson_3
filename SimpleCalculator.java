06971440245efaef28e47cf520c68ba752003230
Marilize Pires <50778867+MarilizePi@users.noreply.github.com>
2019-10-02 19:23:26 -0700

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,54 @@
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getFirstNumber() {
+        return this.firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return this.secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getAdditionResult() {
+        return getFirstNumber() + getSecondNumber();
+    }
+
+    public double getSubtractionResult() {
+        return getFirstNumber() - getSecondNumber();
+    }
+
+    public double getMultiplicationResult() {
+        return getFirstNumber() * getSecondNumber();
+    }
+
+    public double getDivisionResult() {
+        if (getSecondNumber() == 0) {
+            return 0.0;
+        } else ;
+        return getFirstNumber() / getSecondNumber();
+    }
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("Add = " + calculator.getAdditionResult());
+        System.out.println("Subtract = " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("Multiply = " + calculator.getMultiplicationResult());
+        System.out.println("Divide = " + calculator.getDivisionResult());
+
+
+    }
+
+}
